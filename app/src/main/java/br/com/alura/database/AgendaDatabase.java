a9906ff04af8bc00726e15ee3b3e916e180bba74
William Almeida <almeida.ws@hotmail.com>
2022-02-22 21:34:48 -0300

    3th course (Android Room parte 1) - 2.3 - Implementando componentes principais do Room Parte 2

--- app/src/main/java/br/com/alura/database/AgendaDatabase.java
+++ app/src/main/java/br/com/alura/database/AgendaDatabase.java
@@ -1,8 +1,14 @@
 package br.com.alura.database;
 
 import androidx.room.Database;
+import androidx.room.RoomDatabase;
 
-@Database(entities = {}, version = 1)
-public class AgendaDatabase {
+import br.com.alura.model.Aluno;
 
+@Database(
+        entities = {Aluno.class},
+        version = 1,
+        exportSchema = false
+)
+public abstract class AgendaDatabase extends RoomDatabase {
 }
---
+++ app/src/main/java/br/com/alura/database/dao/RoomAlunoDAO.java
@@ -0,0 +1,7 @@
+package br.com.alura.database.dao;
+
+import androidx.room.Dao;
+
+@Dao
+public interface RoomAlunoDAO {
+}
